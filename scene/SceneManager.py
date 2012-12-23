0cde75b46d972f71985646bfec3421e89e1beaae
alexpalagin <alexpalagin92@gmail.com>
2012-12-23 22:26:26 +0300

    added Bezie, Parabola, B-spline and Fill features

--- scene/SceneManager.py
+++ scene/SceneManager.py
@@ -1,99 +1,129 @@
+from __future__ import division
 __author__ = 'apalagin'
 
-import time
 
 from PySide import QtCore
 
+from utils.utils import singleton, MinPointsCountError
 from Scene import GraphicScene
-from algorhitms.Line import Line
-from algorhitms.Curve import Curve
-
-def singleton(cls):
-	instances = {}
-	def getinstance():
-		if cls not in instances:
-			instances[cls] = cls()
-		return instances[cls]
-	return getinstance
+from algorhitms.Line import CDA, Bresenham
+from algorhitms.Curve import Bezie, BSpline, Circle, Parabola
+from algorhitms.Fill import Fill
 
 @singleton
 class SceneManger :
-	def __init__(self):
-		self.__scene = GraphicScene()
-		self.__debugMode = False
-		self.__pixelsToDraw = []
-		self.__debugTimer = QtCore.QTimer()
-		self.__debugTimer.timeout.connect(self.__drawNext)
+    def __init__(self):
+        self.__scene = GraphicScene()
+        self.__debugMode = False
+        self.__pixelsToDraw = []
+        self.__debugTimer = QtCore.QTimer()
+        self.__debugTimer.timeout.connect(self.__drawNext)
 
-	def getScene(self):
-		return self.__scene
+    def getScene(self):
+        return self.__scene
 
-	def setScene(self,scene):
-		if isinstance(scene,GraphicScene):
-			self.__scene = scene
-		raise Exception("must be instance of GraphicScene")
+    def setScene(self,scene):
+        if isinstance(scene,GraphicScene):
+            self.__scene = scene
+        raise Exception("must be instance of GraphicScene")
 
-	def setDebugMode(self,mode):
-		self.__debugMode = mode
+    def setDebugMode(self,mode):
+        self.__debugMode = mode
 
-	def isDebugMode(self):
-		return self.__debugMode
+    def isDebugMode(self):
+        return self.__debugMode
 
-	def redrawGrid(self):
-		self.__scene.clearGrid()
-		self.__scene.drawGrid()
-		self.__scene.update()
+    def redrawGrid(self):
+        self.__scene.clearGrid()
+        self.__scene.drawGrid()
+        self.__scene.update()
 
-	def setNewPixelSize(self,pixelSize):
-		if not pixelSize == self.__scene.getPixelSize():
-			self.__scene.setPixelSize(pixelSize)
-			self.__scene.clearEndingPoints()
-			self.__scene.clearSceneFromPixels()
-			self.redrawGrid()
+    def setNewPixelSize(self,pixelSize):
+        if not pixelSize == self.__scene.getPixelSize():
+            self.__scene.setPixelSize(pixelSize)
+            self.__scene.clearEndingPoints()
+            self.__scene.clearSceneFromPixels()
+            self.redrawGrid()
 
-	def getPixelSize(self):
-		return self.__scene.getPixelSize()
+    def getPixelSize(self):
+        return self.__scene.getPixelSize()
 
-	def clearAll(self):
-		self.__scene.clearEndingPoints()
-		self.__scene.clearSceneFromPixels()
-		self.__pixelsToDraw = []
+    def clearAll(self):
+        self.__scene.clearEndingPoints()
+        self.__scene.clearSceneFromPixels()
+        self.__pixelsToDraw = []
 
-	def drawDDA(self):
-		points =  self.__scene.getEndingPointsPos()
-		self.__pixelsToDraw  += Line.CDA(points)
-		self.__drawPixels()
+    def __drawPixels(self):
+        if self.__debugMode:
+            self.__debugTimer.start(100)
+        else:
+            while self.__pixelsToDraw:
+                self.__drawNext()
 
-	def drawBresenham(self):
-		points =  self.__scene.getEndingPointsPos()
-		self.__pixelsToDraw += Line.Bresenham(points)
-		self.__drawPixels()
+    def __drawNext(self):
+        if self.__pixelsToDraw:
+            pixel = self.__pixelsToDraw.pop()
+            x = round(pixel[0])*self.__scene.getPixelSize()
+            y = round(pixel[1])*self.__scene.getPixelSize()
+            self.__scene.drawPixel(x,y)
+        else:
+            self.__debugTimer.stop()
 
-	def __drawPixels(self):
-		if self.__debugMode:
-			self.__debugTimer.start(250)
-		else:
-			while self.__pixelsToDraw:
-				self.__drawNext()
+    def drawDDA(self, points = []):
+        if not points:
+            points =  self.__scene.getEndingPointsPos()
+        while len(points) >= CDA.getMinPointsCount():
+            self.__pixelsToDraw  += CDA.getPixels(points)
+            points.pop(0)
+        self.__drawPixels()
 
-	def __drawNext(self):
-		if self.__pixelsToDraw:
-			pixel = self.__pixelsToDraw.pop()
-			x = round(pixel[0])*self.__scene.getPixelSize()
-			y = round(pixel[1])*self.__scene.getPixelSize()
-			self.__scene.drawPixel(x,y)
-		else:
-			self.__debugTimer.stop()
+    def drawBresenham(self,points = []):
+        self.__tmp = []
+        if not points:
+            points =  self.__scene.getEndingPointsPos()
+        while len(points) >= Bresenham.getMinPointsCount():
+            self.__tmp += Bresenham.getPixels(points)
+            self.__pixelsToDraw += Bresenham.getPixels(points)
+            points.pop(0)
+        self.__drawPixels()
 
-#	def drawCurve(self):
-#		first,last = self.__scene.getEndingPointsPos()
-#		pixelSize = self.__scene.getPixelSize()
-#		pixels = Curve.Bresenham(first.x()/pixelSize,first.y()/pixelSize,last.x()/pixelSize,last.y()/pixelSize)
-#		for pixel in pixels:
-#			x = round(pixel[0])*pixelSize
-#			y = round(pixel[1])*pixelSize
-#			self.__scene.drawPixel(x,y)
-#			if self.__debugMode:
-#				time.sleep(0.01)
+    def drawCircle(self):
+        points =  self.__scene.getEndingPointsPos()
+        self.__pixelsToDraw += Circle.getPixels(points)
+        self.__drawPixels()
+
+    def drawParabola(self):
+        points = self.__scene.getEndingPointsPos()
+        self.__pixelsToDraw += Parabola.getPixels(points)
+        self.__pixelsToDraw.reverse()
+        self.__drawPixels()
+
+    def drawBezie(self):
+        points = self.__scene.getEndingPointsPos()
+        self.__pixelsToDraw += Bezie.getPixels(points)
+        self.__drawPixels()
+
+    def drawBSpline(self):
+        points = self.__scene.getEndingPointsPos()
+        N = 20
+        curve = BSpline.getPixels(points,len(points) - 1,N)
+        points = map(curve, (i / N for i in xrange(N + 1)))
+        x, y = ([p[j] for p in points] for j in (0, 1))
+        keyPoints = zip([round(_x) for _x in x],[round(_y) for _y in y])
+        self.drawBresenham(keyPoints)
+
+    def drawFill(self):
+        fillPoint = None
+        points = self.__scene.getEndingPointsPos()
+        if len(points) > 1:
+            fillPoint = points.pop()
+        points.append(points[0])
+        defaultColour = self.__scene.getPixelColour()
+        self.drawBresenham(points)
+        pixels = Fill.getPixels(fillPoint or points[0],self.__tmp)
+        self.__pixelsToDraw += pixels
+        self.__scene.setPixelColour(128,0,0)
+        self.__drawPixels()
+        self.__scene.setPixelColour(*(defaultColour))
 
 
--- ui/MenuBar.py
+++ ui/MenuBar.py
@@ -8,42 +8,57 @@ from scene.SceneManager import SceneManger
 
 class MenuBar(QtGui.QMenuBar):
 
-	def __init__(self):
-		super(MenuBar,self).__init__()
-		self.__sceneManager = SceneManger()
-		self.lineMenu = self.addMenu("Line")
-		self.curveMenu = self.addMenu("Curve")
-		self.settingsMenu = self.addMenu("settings")
-		self.lineMenu.addAction("DDA").triggered.connect(self.DDA)
-		self.lineMenu.addAction("Bresenham").triggered.connect(self.Bresenham)
-		self.curveMenu.addAction("Bresenham").triggered.connect(self.test)
-		self.settingsMenu.addAction("set pixel size").triggered.connect(self.pixelSizeMenu)
-		self.settingsMenu.addAction("clear").triggered.connect(self.clearScene)
-		debugMode = self.settingsMenu.addAction("debug mode")
-		debugMode.setCheckable(True)
-		debugMode.triggered.connect(self.debugTrigger)
+    def __init__(self):
+        super(MenuBar,self).__init__()
+        self.__sceneManager = SceneManger()
+        self.lineMenu = self.addMenu("Line")
+        self.curveMenu = self.addMenu("Curve")
+        self.fillMenu = self.addMenu("Fill")
+        self.settingsMenu = self.addMenu("settings")
+        self.lineMenu.addAction("DDA").triggered.connect(self.DDA)
+        self.lineMenu.addAction("Bresenham").triggered.connect(self.Bresenham)
+        self.curveMenu.addAction("Circle").triggered.connect(self.circle)
+        self.curveMenu.addAction("Parabola").triggered.connect(self.parabola)
+        self.curveMenu.addAction("Bezie").triggered.connect(self.bezie)
+        self.curveMenu.addAction("B-Spline").triggered.connect(self.BSpline)
+        self.fillMenu.addAction("Fill").triggered.connect(self.fill)
+        self.settingsMenu.addAction("set pixel size").triggered.connect(self.pixelSizeMenu)
+        self.settingsMenu.addAction("clear").triggered.connect(self.clearScene)
+        debugMode = self.settingsMenu.addAction("debug mode")
+        debugMode.setCheckable(True)
+        debugMode.triggered.connect(self.debugTrigger)
 
-	def pixelSizeMenu(self):
-		newSize, ok = QtGui.QInputDialog.getInteger(self,"pixel size","pixel size:",
-														self.__sceneManager.getPixelSize(),2,50,1)
-		if ok:
-			self.__sceneManager.setNewPixelSize(newSize)
-		print "pixel size menu"
+    def pixelSizeMenu(self):
+        newSize, ok = QtGui.QInputDialog.getInteger(self,"pixel size","pixel size:",
+                                                    self.__sceneManager.getPixelSize(),2,50,1)
+        if ok:
+            self.__sceneManager.setNewPixelSize(newSize)
+        print "pixel size menu"
 
-	def clearScene(self):
-		self.__sceneManager.clearAll()
-		print "clear scene"
+    def clearScene(self):
+        self.__sceneManager.clearAll()
+        print "clear scene"
 
-	def debugTrigger(self):
-		self.__sceneManager.setDebugMode(not self.__sceneManager.isDebugMode())
+    def debugTrigger(self):
+        self.__sceneManager.setDebugMode(not self.__sceneManager.isDebugMode())
 
+    def Bresenham(self):
+        self.__sceneManager.drawBresenham()
 
-	def Bresenham(self):
-		self.__sceneManager.drawBresenham()
+    def DDA(self):
+        self.__sceneManager.drawDDA()
 
-	def DDA(self):
-		self.__sceneManager.drawDDA()
+    def circle(self):
+        self.__sceneManager.drawCircle()
 
+    def parabola(self):
+        self.__sceneManager.drawParabola()
 
-	def test(self):
-		self.__sceneManager.drawCurve()
+    def bezie(self):
+        self.__sceneManager.drawBezie()
+
+    def BSpline(self):
+        self.__sceneManager.drawBSpline()
+
+    def fill(self):
+        self.__sceneManager.drawFill()
