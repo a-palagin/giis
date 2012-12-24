8fab35e5e3aeda9894efe7f86faeef52b1fae6b2
alexpalagin <alexpalagin92@gmail.com>
2012-12-24 20:19:39 +0300

    added new representation of pixel and conversation method for backward compatibility

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,30 @@
 __author__ = 'drain'
 
+class Color:
+
+    def __init__(self, red, green, blue):
+        self.r = red
+        self.g = green
+        self.b = blue
+
+
+class Colors:
+
+    red = Color(255, 0, 0)
+    green = Color(0, 255, 0)
+    blue = Color(0, 0, 255)
+    black = Color(0,0,0)
+    white = Color(255,255,255)
+
+
+class Pixel:
+
+    def __init__(self,x, y, color=Colors.black):
+        self.x = x
+        self.y = y
+        self.color = color
+
+
 def singleton(cls):
     instances = {}
     def getinstance():
@@ -7,6 +32,12 @@ def singleton(cls):
             instances[cls] = cls()
         return instances[cls]
     return getinstance
+
+def convert(listOfPixels, color = Colors.black):
+    tmp = []
+    for pixel in listOfPixels:
+        tmp.append(Pixel(*pixel,color = color))
+    return tmp
 
 
 class MinPointsCountError(Exception):
